45fa60907419521b647759fd8999709c3804f7a0
kingsman142 <kingsman142@yahoo.com>
2017-09-24 16:40:24 -0400

    finished iddfs for monitoring

--- project1/puzzlesolver.py
+++ project1/puzzlesolver.py
@@ -25,28 +25,87 @@ def execute_search(filename, keyword):
     elif input[0] == "aggregation":
         aggregation(input, keyword)
 
-def bfs(init_state, actions, goal_state, transition):
+
+
+def iddfs(init_state, actions, goal_state, transition, unique_value):
+    goal_states = []
+    nodes_created = 0
+    max_frontier_size = 0
+    max_explored_size = 0
+    for i in xrange(0, 100):
+        info = dfs(init_state, actions, goal_state, set([]), transition, unique_value, 0, i, 1, 0, 0)
+        if info:
+            nodes_created += info[1]
+            max_frontier_size = max(max_frontier_size, info[2])
+            max_explored_size = max(max_explored_size, info[3])
+            for state in info[0]: # for state in goal states
+                goal_states.append(state)
+    #print("goal states len: " + str(len(goal_states)))
+    max_cost = 0
+    state = ()
+    #print(goal_states)
+    for goal_state in goal_states:
+        #print(goal_state[3])
+        if goal_state[3] > max_cost:
+            state = goal_state
+            max_cost = goal_state[3]
+    #print("MAX COST: " + str(max_cost))
+    return (state, nodes_created, (max_frontier_size, max_explored_size), max_cost) # end state, time, space, cost
+
+def dfs(state, actions, goal_state, explored, transition, unique_value, depth, max_depth, nodes_created, frontier_size, max_frontier_size):
+    state_unique_identifier = unique_value(state)
+    if goal_state(state):
+        return ([state], nodes_created, max_frontier_size, explored)
+    elif not frozenset(state_unique_identifier) in explored:
+        explored.add(frozenset(state_unique_identifier))
+        if depth < max_depth:
+            goal_states = []
+            for action in actions:
+                new_state = transition(action, state)
+                for state in new_state:
+                    frontier_size -= 1
+                    #print(max_frontier_size)
+                    return_value = dfs(state, actions, goal_state, explored, transition, unique_value, depth+1, max_depth, nodes_created+len(new_state), frontier_size+len(new_state), max(max_frontier_size, frontier_size+len(new_state)))
+                    if not return_value is None:
+                        for return_state in return_value[0]:
+                            #print(return_value)
+                            #print(return_state)
+                            #goal_states.add(return_state)
+                            goal_states.append(return_state)
+                        #print(return_value)
+                        max_frontier_size = max(max_frontier_size, return_value[2])
+            return (goal_states, nodes_created, max_frontier_size, explored)
+    return None
+
+def bfs(init_state, actions, goal_state, transition, unique_value):
     frontier = []
+    explored = set([])
     frontier.append(init_state)
     goal_states = []
     time = 1 # We already created 1 node, the initial state
     frontier_space = 1 # Maximum amount of space the frontier grew to
-    visited = 1 # Number of nodes we have visited
+    visited = 0 # Number of nodes we have visited
     print("bfs frontier size: " + str(len(frontier)))
 
     while frontier:
         curr_state = frontier.pop(0) # Pop from the left of the list
-        visited += 1
+        state_unique_identifier = unique_value(curr_state)
+        #visited += 1
         if goal_state(curr_state):
             #print(curr_state)
             goal_states.append(curr_state)
-        for action in actions:
-            #print(str(curr_state))
-            new_state = transition(action, curr_state)
-            for state in new_state:
-                time += 1
-                frontier.append(state)
-            frontier_space = max(len(frontier), frontier_space)
+        elif not frozenset(state_unique_identifier) in explored: # If we haven't explored this state yet
+            #print(frozenset(state_unique_identifier))
+            explored.add(frozenset(state_unique_identifier))
+            visited += 1
+            for action in actions:
+                #print(str(curr_state))
+                new_state = transition(action, curr_state)
+                for state in new_state:
+                    time += 1
+                    frontier.append(state)
+                frontier_space = max(len(frontier), frontier_space)
+                #print("frontier: " + str(frontier_space))
 
     max_cost = -sys.maxint
     goal_state = ()
@@ -70,7 +129,7 @@ def find_next_target_sensor(action, state):
             for target in do_not_modify_targets: # This is why we needed this immutable list, so we can scan over it later and find every target easier.
                 # Make a copy of the sensors and output in the current state, then remove the current sensor because we're adding it to the output pairs list.
                 sensor_copy = list(sensors)
-                output_copy = list(output)
+                output_copy = set(output)
                 sensor_copy.remove(sensor)
 
                 # Grab the x and y coordinates of the sensor and target, then calculate their distance and find the sensor's power.
@@ -83,7 +142,7 @@ def find_next_target_sensor(action, state):
 
                 # This is a new pair in our output for the solution.
                 new_pair = (sensor, target, cost)
-                output_copy.append(new_pair)
+                output_copy.add(new_pair)
 
                 # Now that we added another sensor to a target, we have to calculate the local cost for each target, then the lowest cost for
                 # the entire sensor and target dataset.
@@ -107,7 +166,7 @@ def find_next_target_sensor(action, state):
                 # Make a copy of the sensors and output in the current state, then remove the current sensor and target because we're adding them to the output pairs list.
                 sensor_copy = list(sensors)
                 target_copy = list(targets)
-                output_copy = list(output)
+                output_copy = set(output)
                 sensor_copy.remove(sensor)
                 target_copy.remove(target)
 
@@ -121,7 +180,7 @@ def find_next_target_sensor(action, state):
 
                 # This is a new pair (permutation of sensor/target) generated for our solution.
                 new_pair = (sensor, target, cost)
-                output_copy.append(new_pair)
+                output_copy.add(new_pair)
 
                 # Straightforward; we are just generating a new permutation of sensors assigned to targets with their updated cost
                 new_state = (sensor_copy, target_copy, output_copy, min(state[3], cost), do_not_modify_targets)
@@ -134,6 +193,9 @@ def euclidian_distance(x_1, y_1, x_2, y_2):
     x_dist = math.pow(x_2 - x_1, 2)
     y_dist = math.pow(y_2 - y_1, 2)
     return math.sqrt(x_dist + y_dist)
+
+def monitor_unique_value(curr_state):
+    return curr_state[2] # Return the already formed set of sensor-target pairs
 
 # In the monitoring problem, we have reached a goal state if there are no more sensors or targets to be assigned
 def monitor_goal_test(curr_state):
@@ -154,7 +216,7 @@ def monitor(search_info, search_type): # search_info is a list of the sensors ID
     if search_type == "bfs":
         init_state = (sensor_info, target_info, [], sys.maxint, target_info) # sensors, targets, sensor-target pairs, cost, another copy of targets list (DO NOT MANIPULATE THIS)
         actions = [""]
-        results = bfs(init_state, actions, monitor_goal_test, find_next_target_sensor)
+        results = bfs(init_state, actions, monitor_goal_test, find_next_target_sensor, monitor_unique_value)
         for pair in results[0][2]:
             print(pair[0][0] + " monitors " + pair[1][0])
         print("Time: " + str(results[1]))
@@ -165,7 +227,15 @@ def monitor(search_info, search_type): # search_info is a list of the sensors ID
     elif search_type == "greedy":
         pass
     elif search_type == "iddfs":
-        pass
+        init_state = (sensor_info, target_info, [], sys.maxint, target_info)
+        actions = [""]
+        results = iddfs(init_state, actions, monitor_goal_test, find_next_target_sensor, monitor_unique_value)
+        #print(results[0])
+        for state in results[0][2]:
+            print(state[0][0] + " monitors " + state[1][0])
+        print("Time: " + str(results[1]))
+        print("Space: Frontier " + str(results[2][0]) + ", Visited " + str(len(results[2][1])))
+        print("Cost: " + str(results[3]))
     elif search_type == "Astar":
         pass
     return
