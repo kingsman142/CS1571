b89eb737f0cc818165a0d9052610b4ced774c294
kingsman142 <kingsman142@yahoo.com>
2017-11-12 12:51:00 -0500

    changed code from proof by resolution to forward chaining

--- project3/FOL_FC.py
+++ project3/FOL_FC.py
@@ -1,14 +1,12 @@
 import sys
 import os
 
+SYMBOLS = ["^", "v", "->"]
+
 class Clause:
     def __init__(self, fact):
         self.fact = fact
-        self.negation = False
         self.variables = []
-        if fact[0] == "~":
-            self.negation = True
-            self.fact = self.fact.strip("~")
 
         left_paren_index = self.fact.find("(")
         right_paren_index = self.fact.find(")")
@@ -16,17 +14,8 @@ class Clause:
         self.variables = args.split(",")
         self.fact = self.fact[0:left_paren_index]
 
-    def negated(self):
-        return self.negation
-
     def str(self):
-        if self.negation:
-            return "~" + self.fact + "(" + ",".join(self.variables) + ")"
-        else:
-            return self.fact + "(" + ",".join(self.variables) + ")"
-
-    def negate(self):
-        self.negation = not self.negation
+        return self.fact + "(" + ",".join(self.variables) + ")"
 
 class Predicate:
     def __init__(self, logic):
@@ -34,72 +23,59 @@ class Predicate:
 
     def parse_logic(self, logic):
         self.clauses = filter(None, logic.split(" ")) # Filter function removes '' items from list
+        #print("clauses: " + str(self.clauses))
         if "->" in self.clauses:
-            self.clauses = self.convert_rule_to_clauses(self.clauses)
-        elif "PROVE" in self.clauses:
-            self.clauses = self.convert_to_assumption(self.clauses)
+            self.LHS, self.RHS = self.convert_rule_to_clauses(self.clauses)
         else:
-            pass
+            self.LHS = self.clauses
+            self.RHS = []
 
         new_clauses = []
+        self.LHS_objects = []
+        self.RHS_objects = []
+        still_in_LHS = True
         for clause in self.clauses:
-            if not clause in ["^", "v"]:
+            if not clause in SYMBOLS:
                 new_clause = Clause(clause)
                 new_clauses.append(new_clause)
+                if clause in self.LHS:
+                    self.LHS_objects.append(new_clause)
+                else:
+                    self.RHS_objects.append(new_clause)
             else:
                 new_clauses.append(clause)
+                if not clause == "->":
+                    if still_in_LHS:
+                        self.LHS_objects.append(clause)
+                    else:
+                        self.RHS_objects.append(clause)
+                else:
+                    still_in_LHS = False
 
         self.clauses_objects = new_clauses
+        #print("objects: " + str(self.clauses_objects))
 
     def convert_rule_to_clauses(self, rule):
         index_of_implication = rule.index("->")
-        #print("index: " + str(index_of_implication))
-        for i in xrange(0, index_of_implication):
-            if rule[i] == "^":
-                rule[i] = "v"
-            elif rule[i] == "v":
-                rule[i] = "^"
-            else: # Current index is a predicate
-                rule[i] = "~" + rule[i]
-        rule[index_of_implication] = "v"
-        return rule
+        LHS = rule[ : index_of_implication]
+        RHS = rule[index_of_implication : len(rule)+1]
+        return (LHS, RHS)
 
-    def convert_to_assumption(self, statement):
-        return ["~" + statement[1]]
-
-    def find_negation(self, predicate):
+    def find_match(self, predicate):
         for clause in self.clauses_objects:
-            if not clause in ["^", "v"]:
-                if predicate.negated() and not clause.negated():
-                    if predicate.fact == clause.fact:
-                        self.unify(self.clauses_objects, clause, predicate)
-                        i = 0
-                        while i < len(self.clauses_objects):
-                            if isinstance(self.clauses_objects[i], Clause) and self.clauses_objects[i].fact == predicate.fact:
-                                if i < len(self.clauses_objects)-1:
-                                    del self.clauses_objects[i+1]
-                                    del self.clauses_objects[i]
-                                elif i > 0:
-                                    del self.clauses_objects[i]
-                                    del self.clauses_objects[i-1]
-                                else:
-                                    del self.clauses_objects[i]
-                            i += 1
-                elif clause.negated() and not predicate.negated():
-                    if clause.fact == predicate.fact:
-                        self.unify(self.clauses_objects, clause, predicate)
-                        i = 0
-                        while i < len(self.clauses_objects):
-                            if isinstance(self.clauses_objects[i], Clause) and self.clauses_objects[i].fact == predicate.fact:
-                                if i < len(self.clauses_objects)-1:
-                                    del self.clauses_objects[i+1]
-                                    del self.clauses_objects[i]
-                                elif i > 0:
-                                    del self.clauses_objects[i]
-                                    del self.clauses_objects[i-1]
-                                else:
-                                    del self.clauses_objects[i]
-                            i += 1
+            if not clause in SYMBOLS:
+                if predicate.fact == clause.fact:
+                    self.unify(self.clauses_objects, clause, predicate)
+                    if self.is_satisfied():
+                        self.clauses_objects = self.RHS_objects
+
+    def is_satisfied(self):
+        for clause in self.clauses_objects:
+            if clause not in SYMBOLS:
+                for variable in clause.variables:
+                    if not self.is_instance(variable):
+                        return False
+        return True
 
     def unify(self, clauses, clause, predicate):
         clause_var = clause.variables
@@ -113,7 +89,7 @@ class Predicate:
                     substitutions[clause_var[i]] = predicate_var[i]
 
         for curr_clause in clauses:
-            if not curr_clause in ["^", "v"]:
+            if not curr_clause in SYMBOLS:
                 for i in xrange(0, len(curr_clause.variables)):
                     variable = curr_clause.variables[i]
                     if variable in substitutions:
@@ -128,10 +104,11 @@ class Predicate:
     def str(self):
         output = ""
         for clause in self.clauses_objects:
-            if clause in ["^", "v"]:
+            if clause in SYMBOLS:
                 output += clause + " "
             else:
                 output += clause.str() + " "
+        output = output.rstrip()
         return output
 
 def read_input(test_file):
@@ -144,38 +121,57 @@ def create_statements(logic):
     predicates = []
     line_number = 1
     prev_begin_line_number = 1
+    goal = None
+
+    rules = []
+    facts = []
 
     for predicate in logic:
-        new_predicate = Predicate(predicate)
-        predicates.append(new_predicate)
-        print(str(line_number) + ". " + new_predicate.str() + " --  Given")
+        if "PROVE" in predicate:
+            goal = filter(None, predicate.split(" "))[1]
+            print(str(line_number) + ". Goal: " + goal)
+        else:
+            new_predicate = Predicate(predicate)
+            predicates.append(new_predicate)
+            if "->" in predicate:
+                rules.append(new_predicate)
+            else:
+                facts.append(new_predicate)
+            print(str(line_number) + ". " + new_predicate.str() + " --  Given")
         line_number += 1
 
-    while len(predicates) > 1:
+    found_goal = False
+    # TODO: SPLIT UP PREDICATES FROM FACTS/ATOMIC STATEMENTS SO WE KNOW WHEN TO STOP ONCE ALL FACTS ARE DEPLETED (create 'rules' and 'facts' lists)
+    while len(predicates) > 1 and not found_goal:
         new_predicates = []
         for i in xrange(0, len(predicates)):
             pred_original = predicates[i].str()
-            for j in xrange(i+1, len(predicates)):
+            for j in xrange(0, len(predicates)):
                 if not predicates[i] == predicates[j] and len(predicates[j].get_predicate()) == 1:
                     pred_before = predicates[i].str()
-                    predicates[i].find_negation(predicates[j].get_predicate()[0])
+                    predicates[i].find_match(predicates[j].get_predicate()[0])
                     if not pred_before == predicates[i].str():
-                        if len(predicates[i].str()) == 0:
-                            #print(str(line_number) + ". {} --  Resolve from " + str(prev_begin_line_number + i) + " and " + str(prev_begin_line_number + j))
-                            print(str(line_number) + ". {} " + predicates[i].str())
-                        else:
+                        if len(predicates[i].str()) > 0:
                             #print(str(line_number) + ". " + predicates[i].str() + " --  Resolve from " + str(prev_begin_line_number + i) + " and " + str(prev_begin_line_number + j))
                             print(str(line_number) + ". " + predicates[i].str())
                         line_number += 1
             if len(predicates[i].str()) > 0 and not pred_original == predicates[i].str():
+                if predicates[i].str() == goal:
+                    found_goal = True
+                    #print("pred: " + predicates[i].str() + " and goal: " + goal + " MATCH!")
+                    break
                 new_predicates.append(predicates[i])
+
+            if found_goal:
+                break
+
         predicates = new_predicates
         prev_begin_line_number = line_number
 
-    if len(predicates) == 0:
-        print("\nDue to our final statement, we have arrived at an empty predicate. \nOur initial assumption is false, so our proof is correct!")
+    if found_goal:
+        print("\nDue to our final statement, we have arrived at our goal. \nTherefore, our proof is correct!")
     else:
-        print("\nWe have arrived at a non-empty predicate. \nOur initial assumption is true.  Therefore, our proof is incorrect!")
+        print("\nUnfortunately, our proof is incorrect!")
 
     return predicates
 
